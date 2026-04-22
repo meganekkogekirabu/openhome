f527ab519f2fdae4c47d7b3e0463fe64f0196898
Choi Madeleine <gankiann.niu@gmail.com>
2026-04-22 05:20:27 -0400

    feat: implement listing as a subcommand

--- src/main.c
+++ src/main.c
@@ -5,14 +5,24 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "discovery.h"
 
+[[noreturn]] void
+usage()
+{
+  fprintf(stderr, "Copyright (C) 2026 Madeleine Choi\n"
+                  "\n"
+                  "usage: openhome list\n");
+  exit(EXIT_FAILURE);
+}
+
 int
-main(void)
+cmd_list(void)
 {
   all_services_t *all = all_services_discover();
-  
+
   if (!all)
     return EXIT_FAILURE;
 
@@ -25,4 +35,20 @@ main(void)
   free(all);
 
   return EXIT_SUCCESS;
+}
+
+int
+main(int argc, const char **argv)
+{
+  if (argc < 2)
+    usage();
+
+  int ret = EXIT_SUCCESS;
+
+  if (strcmp(argv[1], "list") == 0)
+    cmd_list();
+  else
+    usage();
+
+  return ret;
 }
